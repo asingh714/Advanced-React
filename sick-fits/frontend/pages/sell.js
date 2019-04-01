45c77adb038547c8d410722ee0f6cc7bd64456cb
Aman <amsingh714@gmail.com>
2019-04-01 17:00:26 -0400

    created home and sell pages

---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link"
+
+const Sell = props => {
+  return (
+    <div>
+      <p>Sell!</p>
+      <Link href="/">
+          <a>Home!</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Sell;
