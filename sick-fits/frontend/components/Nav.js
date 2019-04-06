4df85066a6c35aaa9e2826795bbf16ea22c0bb78
Aman <amsingh714@gmail.com>
2019-04-06 12:28:36 -0400

    added styles to nav

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,15 +1,16 @@
 import Link from "next/link";
+import NavStyles from "./styles/NavStyles";
 
 const Nav = () => {
   return (
-    <div>
+    <NavStyles>
       <Link href="/sell">
         <a>Sell!</a>
       </Link>
       <Link href="/">
           <a>Home!</a>
       </Link>
-    </div>
+    </NavStyles>
   );
 };
 
